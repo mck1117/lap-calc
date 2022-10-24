c560d6a39ba80b7eb53af4ab10ce56f423ff1271
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-24 15:24:47 -0700

    lap reader -> log reader

--- lap_calc/LogReader.cs
+++ lap_calc/LogReader.cs
@@ -21,7 +21,7 @@ namespace lap_calc
         public CarPoint ParsedCarPoint;
     }
 
-    internal class LapReader
+    internal class LogReader
     {
         private static int FindColumnIndex(string[] headers, string name)
         {
@@ -85,7 +85,7 @@ namespace lap_calc
             throw new InvalidDataException();
         }
 
-        public static IEnumerable<LogLineRead> ReadLaps(String file, ICoordinateConverter converter)
+        public static IEnumerable<LogLineRead> ReadLog(String file, ICoordinateConverter converter)
         {
             using (StreamReader sr = new StreamReader(file))
             {
--- lap_calc/Program.cs
+++ lap_calc/Program.cs
@@ -9,74 +9,6 @@ using DotSpatial.Projections.Transforms;
 
 namespace lap_calc
 {
-    public interface ICoordinateConverter
-    {
-        Vector2 ToLocal(double lat, double lon);
-    }
-
-    public class UtmConverter : ICoordinateConverter
-    {
-        private double OffsetX { get; init; }
-        private double OffsetY { get; init; }
-
-        public UtmConverter(double referenceLat, double referenceLon)
-        {
-            var offset = Project(referenceLat, referenceLon);
-            OffsetX = offset[0];
-            OffsetY = offset[1];
-        }
-
-        private double[] Project(double lat, double lon)
-        {
-            var wgs84 = KnownCoordinateSystems.Geographic.World.WGS1984;
-            var utm = KnownCoordinateSystems.Projected.UtmWgs1984.WGS1984UTMZone10N;
-
-            double[] xy = new double[] { lon, lat };
-
-            Reproject.ReprojectPoints(xy, new double[] { 0 }, wgs84, utm, 0, 1);
-
-            return xy;
-        }
-
-        public Vector2 ToLocal(double lat, double lon)
-        {
-            var raw = Project(lat, lon);
-
-            return new Vector2((float)(raw[0] - OffsetX), (float)(raw[1] - OffsetY));
-        }
-    }
-
-    public class SuperCheatyConverter : ICoordinateConverter
-    {
-        private static readonly double EarthCircumferenceMetersPerDegree = 40075017 / 360;
-
-        private double LocalMetersPerLongitude { get; init; }
-
-        private double OffsetLat { get; init; }
-       
-        private double OffsetLon { get; init; }
-
-        public SuperCheatyConverter(double referenceLat, double referenceLon)
-        {
-            // The circumference of the latitude near the track won't change much, so we can precompute it
-            this.LocalMetersPerLongitude = EarthCircumferenceMetersPerDegree * Math.Cos(referenceLat * Math.PI / 180);
-
-            this.OffsetLon = referenceLon;
-            this.OffsetLat = referenceLat;
-        }
-
-        public Vector2 ToLocal(double lat, double lon)
-        {
-            double lonDelta = lon - this.OffsetLon;
-            double x = this.LocalMetersPerLongitude * lonDelta;
-
-            double latDelta = lat - this.OffsetLat;
-            double y = EarthCircumferenceMetersPerDegree * latDelta;
-
-            return new Vector2((float)x, (float)y);
-        }
-    }
-
     class Program
     {
         public static IEnumerable<string> FileLines(string file)
@@ -141,7 +73,7 @@ namespace lap_calc
 
             //FindTrackPosition2(new Vector2(0.8f, 0.9f));
 
-            var carPoints = LapReader.ReadLaps(args[1], converter);
+            var carPoints = LogReader.ReadLog(args[1], converter);
 
             StreamWriter sw = new StreamWriter(args[2]);
 
